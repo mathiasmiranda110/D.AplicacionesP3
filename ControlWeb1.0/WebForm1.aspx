808853aa253c15e9ed8523bd264e6a6afdfd82a3
Mathias Jose Miranda Rivera <mathiasmiranda110@gmail.com>
2021-09-10 16:27:02 -0500

    proyecto 1 experiencia 1

---
+++ ControlWeb1.0/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ControlWeb1._0.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        &nbsp;<asp:Label ID="Ctrl" runat="server" BorderStyle="Dashed" Text="Test Border"></asp:Label>
+            <br />
+            <br />
+&nbsp;<asp:CheckBoxList ID="chklst" runat="server">
+            </asp:CheckBoxList>
+            <br />
+&nbsp;<br />
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="cmdOK" runat="server" Text="Button" OnClick="cmdOK_Click" />
+        
+            <br />
+            <br />
+            <asp:Label ID="lblResult" runat="server" Text="Label"></asp:Label>
+            <br />
+        
+        </div>
+    </form>
+</body>
+</html>
---
+++ ControlWeb1.0/WebForm1.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ControlWeb1._0
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Ctrl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Ctrl;
+
+        /// <summary>
+        /// Control chklst.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList chklst;
+
+        /// <summary>
+        /// Control cmdOK.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdOK;
+
+        /// <summary>
+        /// Control lblResult.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblResult;
+    }
+}
